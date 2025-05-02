bd87c52b82b5206b6969fd939b96ffc7bf6189f6
Jagger <jagger@defi.sucks>
2025-05-02 15:27:05 -0300

    feat: enhance HumanReadableAddress type with checksum validation format

--- packages/addresses/src/types/humanReadableAddress.ts
+++ packages/addresses/src/types/humanReadableAddress.ts
@@ -1,5 +1,7 @@
+import type { Checksum } from "../internal.js";
+
 /**
  * Represents an interop address in a human readable format.
  * @example 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045@eip155:1#4CA88C9C
  */
-export type HumanReadableAddress = string;
+export type HumanReadableAddress = `${string}@${string}:${string}#${Checksum}`;
