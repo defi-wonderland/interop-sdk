57d3f7f96c5b3b964a76af8d9d72e25c010de8d8
0xjagger <jagger@defi.sucks>
2025-05-08 12:04:59 -0300

    feat: add TODOs for utility methods in InteropAddressProvider class

--- packages/addresses/src/providers/InteropAddressProvider.ts
+++ packages/addresses/src/providers/InteropAddressProvider.ts
@@ -1,12 +1,16 @@
 export class InteropAddressProvider {
     private constructor() {} // prevent instantiation
 
+    // TODO: Implement
     public static humanReadableToBinary(): void {}
 
+    // TODO: Implement
     public static binaryToHumanReadable(): void {}
 
+    // TODO: Implement
     public static getChainId(): void {}
 
+    // TODO: Implement
     public static getAddress(): void {}
 }
 
