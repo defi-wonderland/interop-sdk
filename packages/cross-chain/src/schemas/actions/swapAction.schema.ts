d5caed6c0ebec526b26cb870595bb1a58f579678
0xAaCE <0xAaCE@proton.me>
2025-05-13 11:05:25 -0300

    chore: use isAddress method

--- packages/cross-chain/src/schemas/actions/swapAction.schema.ts
+++ packages/cross-chain/src/schemas/actions/swapAction.schema.ts
@@ -1,10 +1,15 @@
+import { isAddress } from "viem";
 import { z } from "zod";
 
 export const SwapGetQuoteParamsSchema = z.object({
     inputAmount: z.string(),
     outputAmount: z.string(),
-    inputTokenAddress: z.string(),
-    outputTokenAddress: z.string(),
+    inputTokenAddress: z.string().refine((val) => isAddress(val), {
+        message: "Invalid input token address",
+    }),
+    outputTokenAddress: z.string().refine((val) => isAddress(val), {
+        message: "Invalid output token address",
+    }),
     inputChainId: z.string(),
     outputChainId: z.string(),
     slippage: z.string(),
