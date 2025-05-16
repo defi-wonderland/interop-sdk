ceea53966529aa99bc3a6e9a376e441153430876
0xjagger <jagger@defi.sucks>
2025-05-16 11:17:11 -0300

    feat: add chain shortname mapping and fetch functionality

---
+++ packages/addresses/src/constants/chainShortnameMap.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+import { DEFAULT_CHAIN_SHORTNAME_MAP } from "./defaultShortnameMap.js";
+
+/**
+ * Fetches the shortname to chain id map from the chainid.network API.
+ *
+ * @returns {Promise<Record<string, number>>} The shortname to chain id map.
+ */
+const fetchShortnameToChainId = async (): Promise<Record<string, number>> => {
+    try {
+        const response = await axios.get("https://chainid.network/chains_mini.json");
+        const chains = response.data;
+        const shortNameToChainIdObj = chains.reduce((acc: Record<string, number>, chain: any) => {
+            if (chain.shortName && chain.chainId != null) {
+                acc[chain.shortName] = chain.chainId;
+            }
+            return acc;
+        }, {});
+
+        return shortNameToChainIdObj;
+    } catch (error) {
+        console.error("Error fetching shortname to chain id", error);
+        return {};
+    }
+};
+
+// Only fetch the shortname to chain id map if we're not running in test mode
+let CHAIN_SHORTNAME_TO_ID_MAP: Record<string, number> = DEFAULT_CHAIN_SHORTNAME_MAP;
+if (process.env.NODE_ENV !== "test") {
+    CHAIN_SHORTNAME_TO_ID_MAP = (await fetchShortnameToChainId()) || DEFAULT_CHAIN_SHORTNAME_MAP;
+}
+
+export { CHAIN_SHORTNAME_TO_ID_MAP };
