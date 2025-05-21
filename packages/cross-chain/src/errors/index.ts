bf479ddb60510c9024d27f06880bba48ffa31688
0xjagger <jagger@defi.sucks>
2025-05-21 17:04:18 -0300

    fix: correct typo in UnsupportedProtocol export

--- packages/cross-chain/src/errors/index.ts
+++ packages/cross-chain/src/errors/index.ts
@@ -2,4 +2,4 @@ export * from "./GetTokenAllowanceIssue.exception.js";
 export * from "./GetTokenDecimalsIssue.exception.js";
 export * from "./UnsupportedAction.exception.js";
 export * from "./UnsupportedChainId.exception.js";
-export * from "./UnsupportedProtocol.expcetion.js";
+export * from "./UnsupportedProtocol.exception.js";
