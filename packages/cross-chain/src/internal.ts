83c172839b4d0e21ac7d9f52b407a92248fa8096
0xAaCE <0xAaCE@proton.me>
2025-05-21 12:06:45 -0300

    chore: remove double export

--- packages/cross-chain/src/internal.ts
+++ packages/cross-chain/src/internal.ts
@@ -1,7 +1,5 @@
 export * from "./constants/index.js";
 export * from "./interfaces/index.js";
-export * from "./constants/index.js";
-export * from "./interfaces/index.js";
 export * from "./providers/index.js";
 export * from "./schemas/index.js";
 export * from "./utils/index.js";
