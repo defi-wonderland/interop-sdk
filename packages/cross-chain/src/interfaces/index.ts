bd58b2901160fe7296df2f6fe0581240e3045c30
0xjagger <jagger@defi.sucks>
2025-05-22 14:44:38 -0300

    refactor: add AcrossProvider interface and AcrossTransferOpenParams schema

--- packages/cross-chain/src/interfaces/index.ts
+++ packages/cross-chain/src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export * from "./crossChainProvider.interface.js";
 export * from "./actions/index.js";
+export * from "./AcrossProvider.interface.js";
--- packages/cross-chain/src/schemas/index.ts
+++ packages/cross-chain/src/schemas/index.ts
@@ -1,2 +1,3 @@
 export * from "./actions/index.js";
 export * from "./chain.js";
+export * from "./AcrossTransferOpenParams.schema.js";
