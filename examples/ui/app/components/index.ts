e12e2edb83c5c1936ca33e878d9880e7654eb378
th0rOdinson <144699868+th0rOdinson@users.noreply.github.com>
2025-11-20 13:50:16 -0300

    feat: footer

---
+++ examples/ui/app/components/index.ts
@@ -0,0 +1,4 @@
+export { Footer } from './Footer';
+export { InteractivePlayground } from './InteractivePlayground';
+export { ThemeSwitcher } from './ThemeSwitcher';
+export { WonderlandLogotype } from './WonderlandLogotype';
